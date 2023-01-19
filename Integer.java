707441b0dfb27498e1f3985e6bc78da67dafe27d
Teena Kumari <supriya193531@dei.ac.in>
2023-01-19 14:59:25 +0530

    First Code

---
+++ Integer.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Integer {
+    public static void main(String[] args) {
+        Scanner Sc = new Scanner(System.in);
+        int A = Sc.nextInt();
+        boolean A;
+        System.out.println(C);
+    }
+
+}
