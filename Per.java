0e9c9c296acfca1d8dd98871fcf61e74a6de2c16
Teena Kumari <supriya193531@dei.ac.in>
2023-01-19 14:57:48 +0530

    First Commit

---
+++ Per.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Per {
+
+    public static void main(String[] args) {
+        System.out.println("type  the value");
+        Scanner SC = new Scanner(System.in);
+
+        int a = SC.nextInt();
+        int b = SC.nextInt();
+        int c = SC.nextInt();
+        int d = SC.nextInt();
+        int e = SC.nextInt();
+        int oT = a + b + c + d + e;
+        System.out.println("total of five sub marks" + oT);
+        int p = oT / 5;
+        System.out.println("percentage is" + p + "%");
+    }
+
+}
---
+++ Str.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Str {
+    public static void main(String[] args) {
+        Scanner Sc = new Scanner(System.in);
+        String a = Sc.next();
+        System.out.println(a);
+    }
+
+}
